764b90734cdcc4204d0da97cad7ebc22e59ceb89
Leonardo <vr.leonardo2020@gmail.com>
2024-11-26 11:57:00 -0300

    feat(auth): implement local strategy validation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { v4 as uuidv4 } from 'uuid';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 
@@ -11,8 +10,8 @@ export class UserService {
 async create(createUserDto: CreateUserDto) {
     
   const data = {
-    password: await bcrypt.hash(createUserDto.password, 10),
     ...createUserDto,
+    password: await bcrypt.hash(createUserDto.password, 10),
   };
   const createdUser = await this.prisma.user.create({ data });
   
